5801529b001e308a8a215694860ed5c517e0ba10
hughug27 <lequanghung27122@gmail.com>
2024-01-19 04:50:46 +0700

    2sd

--- app/src/main/java/com/example/laptrinhapp/model/Course.java
+++ app/src/main/java/com/example/laptrinhapp/model/Course.java
@@ -1,6 +1,30 @@
 package com.example.laptrinhapp.model;
 
+import java.lang.reflect.Array;
+import java.util.List;
+
 public class Course {
     private String courseName;
-    private Student student;
+    private List<Student> student;
+
+    public Course(String courseName, List<Student> student) {
+        this.courseName = courseName;
+        this.student = student;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public List<Student> getStudent() {
+        return student;
+    }
+
+    public void setStudent(List<Student> student) {
+        this.student = student;
+    }
 }
